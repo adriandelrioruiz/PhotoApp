9cb5ce3d591012b87d87be60f433572c278d1bf2
adriandelrio <adriandelrioruiz0@gmail.com>
2022-10-29 14:44:28 +0200

    inicie mi repo

---
+++ src/main/java/umu/tds/maven/apps/PhotoApp/Launcher.java
@@ -0,0 +1,17 @@
+package umu.tds.maven.apps.PhotoApp;
+
+import java.util.ArrayList;
+
+import beans.Entidad;
+import tds.driver.FactoriaServicioPersistencia;
+import tds.driver.ServicioPersistencia;
+
+public class Launcher {
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		
+		ServicioPersistencia servPersistencia = FactoriaServicioPersistencia.getInstance().getServicioPersistencia();
+		Entidad eProducto = new Entidad();
+		eProducto = servPersistencia.registrarEntidad(eProducto);
+	}
+}
