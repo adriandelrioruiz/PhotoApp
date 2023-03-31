521c71f39174e932539aad181c44dd9c8f8564c1
IsmaelHA <ismael.h.a@um.es>
2023-03-31 12:08:14 +0200

    Funcion createPDF del controlador terminada

--- src/main/java/umu/tds/maven/apps/PhotoApp/controlador/PhotoAppController.java
+++ src/main/java/umu/tds/maven/apps/PhotoApp/controlador/PhotoAppController.java
@@ -42,6 +42,12 @@ import umu.tds.maven.apps.PhotoApp.persistencia.IUserAdapterDAO;
 import umu.tds.maven.apps.PhotoApp.persistencia.PhotoAdapterTDS;
 import umu.tds.maven.apps.PhotoApp.persistencia.UserAdapterTDS;
 import umu.tds.maven.apps.PhotoApp.vista.pantallaprincipal.LoggedFrame;
+import java.io.FileOutputStream;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.text.Phrase;
 
 public class PhotoAppController {
 
@@ -534,7 +540,23 @@ public class PhotoAppController {
 
 	// TODO
 	public void generatePDF() {
-
+		 	Document document = new Document();
+	        PdfWriter.getInstance(document, new FileOutputStream("seguidores.pdf"));
+	        document.open();
+	        PdfPTable table = new PdfPTable(3); // 3 columnas
+	        PdfPCell cell1 = new PdfPCell(new Phrase("Nombre"));//nombre usuario
+	        PdfPCell cell2 = new PdfPCell(new Phrase("Email"));//email usuario
+	        PdfPCell cell3 = new PdfPCell(new Phrase("Descripción"));//descripción usuario
+	        table.addCell(cell1);
+	        table.addCell(cell2);
+	        table.addCell(cell3);
+	        for (User user : user.getFollowers()) {
+		        table.addCell(user.getUserName());
+		        table.addCell(user.getEmail());
+		        table.addCell(user.getBio());
+	        }
+	        document.add(table);
+	        document.close();
 	}
 
 	public boolean generateExcel(String path) {
